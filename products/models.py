e69b2cd8e1bcf34c91aff6853d488626bd861866
Artem Zubrytskyi <artemzybr1605@gmail.com>
2022-08-09 17:04:10 +0300

    add category to models and admin

--- products/models.py
+++ products/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 
-# Create your models here.
-# class CategoryModel(models.Model):
-#     class CategoryModel(models.Model):
-#         name_category = models.CharField(max_length=30)
-#         slug_category = models.SlugField(unique=True, max_length=30)
-#         description_category = models.TextField()
-#     def __str__(self):
-#         return self.
-#
-#         #
-#     class Meta:
-#         verbose_name = "Robot"
-#         verbose_name_plural = "Robots"
+class CategoryModel(models.Model):
+    name_category= models.CharField(max_length=30)
+    slug_category = models.SlugField(unique=True, max_length=30)
+    description_category = models.TextField()
+    def __str__(self):
+        return self.name_category
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+        ordering = "slug_category",
+        db_table = "products_categorymodel"
 
 class ProductModel(models.Model):
     product_name = models.CharField(max_length=20, blank=True)
@@ -22,6 +20,7 @@ class ProductModel(models.Model):
     product_count = models.PositiveIntegerField(blank=True)
     product_available = models.BooleanField(blank=True)
     product_img = models.URLField(blank=True)
+    category = models.ForeignKey("CategoryModel", on_delete=models.PROTECT,null=True)
 
     def __str__(self):
         return self.product_name
@@ -32,4 +31,6 @@ class ProductModel(models.Model):
         verbose_name_plural = "products"
         ordering = "product_slug",
         db_table = "product_base"
+
+
 
