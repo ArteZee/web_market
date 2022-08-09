bad193cceac9cc2a9fb3e7af9c5280c6c461148a
Artem Zubrytskyi <artemzybr1605@gmail.com>
2022-08-10 00:43:40 +0300

    add style for cat/hompage. Eddit views and connected with data bse

--- products/models.py
+++ products/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 
+
 class CategoryModel(models.Model):
-    name_category= models.CharField(max_length=30)
+    name_category = models.CharField(max_length=30)
     slug_category = models.SlugField(unique=True, max_length=30)
     description_category = models.TextField()
+
     def __str__(self):
         return self.name_category
+
     class Meta:
         verbose_name = "category"
         verbose_name_plural = "categories"
         ordering = "slug_category",
         db_table = "products_categorymodel"
+
 
 class ProductModel(models.Model):
     product_name = models.CharField(max_length=20, blank=True)
@@ -20,17 +24,13 @@ class ProductModel(models.Model):
     product_count = models.PositiveIntegerField(blank=True)
     product_available = models.BooleanField(blank=True)
     product_img = models.URLField(blank=True)
-    category = models.ForeignKey("CategoryModel", on_delete=models.PROTECT,null=True)
+    category = models.ForeignKey("CategoryModel", on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.product_name
-
 
     class Meta:
         verbose_name = "product"
         verbose_name_plural = "products"
         ordering = "product_slug",
         db_table = "product_base"
-
-
-
