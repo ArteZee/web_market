7dec101af044ce9a7bdb620b0e3e0b59382bb629
Artem Zubrytskyi <artemzybr1605@gmail.com>
2022-08-09 15:37:53 +0300

    edit views/models/admin for product.app  eddit product.html

--- products/models.py
+++ products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+# class CategoryModel(models.Model):
+#     class CategoryModel(models.Model):
+#         name_category = models.CharField(max_length=30)
+#         slug_category = models.SlugField(unique=True, max_length=30)
+#         description_category = models.TextField()
+#     def __str__(self):
+#         return self.
+#
+#         #
+#     class Meta:
+#         verbose_name = "Robot"
+#         verbose_name_plural = "Robots"
+
+class ProductModel(models.Model):
+    product_name = models.CharField(max_length=20, blank=True)
+    product_slug = models.SlugField(unique=True, max_length=20, blank=True)
+    product_price = models.PositiveIntegerField(blank=True)
+    product_description = models.TextField(blank=True)
+    product_count = models.PositiveIntegerField(blank=True)
+    product_available = models.BooleanField(blank=True)
+    product_img = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.product_name
+
+
+    class Meta:
+        verbose_name = "product"
+        verbose_name_plural = "products"
+        ordering = "product_slug",
+        db_table = "product_base"
+
