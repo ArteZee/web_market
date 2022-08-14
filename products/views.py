b1817e9cdd165ff289655f18c54a38961747cbc5
ArteZee <artemzybr1605@gmail.com>
2022-08-14 18:39:43 +0300

    edit views and done that he work

--- products/views.py
+++ products/views.py
@@ -4,7 +4,5 @@ from .models import ProductModel
 
 
 def product(request: HttpRequest, product_name) -> HttpResponse:
-    element_product = ProductModel.objects.all()
-    for el in element_product:
-        if el.product_slug == product_name:
-            return render(request, "product.html", {"el": el})
+    context = {"object": ProductModel.objects.get(product_slug=product_name)}
+    return render(request, "product.html", context)
