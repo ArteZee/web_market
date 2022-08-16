e62595193bb61ffe79da47fe004c11162830c3a8
ArteZee <artemzybr1605@gmail.com>
2022-08-16 03:22:28 +0300

    add user - login logout register

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 """User App URL Configuration """
 from django.urls import path
-from user.views import user_profile
-
+from user.views import user_profile, login_view, logout_view, register_view
 
 urlpatterns = [
     path("profile/", user_profile, name="user_profile"),
-
+    path("login/", login_view, name="login"),
+    path("register/", register_view, name="register"),
+    path("logout/", logout_view, name="logout"),
 ]
