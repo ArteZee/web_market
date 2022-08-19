7df35b16c9561ee1fd398a81969c6426031f80d5
ArteZee <artemzybr1605@gmail.com>
2022-08-19 15:53:07 +0300

    set up  login register and update with forms add Custom user add category and product json files

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 """User App URL Configuration """
 from django.urls import path
-from user.views import user_profile, login_view, logout_view, register_view
+from user.views import login_view, logout_view, register_view, change_data_user, user_view
 
+app_name = "user"
 urlpatterns = [
-    path("profile/", user_profile, name="user_profile"),
     path("login/", login_view, name="login"),
     path("register/", register_view, name="register"),
     path("logout/", logout_view, name="logout"),
+    path("<slug:slug>", user_view, name="user"),
+    path("<slug:slug>/update/", change_data_user, name="change-data")
 ]
