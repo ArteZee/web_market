51786d041fcb5444ac14ada9f2c6a83a036c4087
ArteZee <artemzybr1605@gmail.com>
2022-10-02 00:46:43 +0300

    edir order serializers.py

---
+++ order/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import OrderModel
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    """"""
+
+    class Meta:
+        model = OrderModel
+        fields = "__all__"
+
